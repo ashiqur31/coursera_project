3427397290f418f58d7496073b1ad05b4487e958
Ashiqur Rahman <ashiqur31@gmail.com>
2025-02-07 12:57:32 +0530

    fixes

--- javascript_project.js
+++ javascript_project.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navbarPlaceholder = document.getElementById("navbar");
   if (navbarPlaceholder) {
-    fetch("/navbar/navbar.html")
+    fetch("/navbar.html")
       .then((response) => response.text())
       .then((data) => {
         navbarPlaceholder.innerHTML = data;
